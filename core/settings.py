8b977a50a419f7686648785f68227022a234c971
oyebodemax <bodokunfolami@gmail.com>
2023-04-19 20:14:01 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -132,14 +132,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "uxlivinglab@dowellresearch.sg"
-EMAIL_HOST_PASSWORD = "gudfkmmoaaeyjzvx"
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
