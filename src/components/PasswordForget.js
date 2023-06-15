bab295b4292345b36f5a5f1bde2dea91b68a4990
Addisu <addisuhaile87@gmail.com>
2023-06-15 08:57:35 +0300

    Update forget password component

--- src/components/PasswordForget.js
+++ src/components/PasswordForget.js
@@ -29,38 +29,39 @@ const PasswordForget = () => {
         <form>
           <div className="overflow-hidden drop-shadow-2xl sm:rounded-2xl bg-amber-100">
             <div className="px-4 py-2 sm:p-6 space-y-4">
-              <div className="">
+              <div className="sm:col-span-2">
                 <label
-                  className="block text-gray-700 text-sm font-medium mb-2"
-                  for="name"
+                  htmlFor="name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
                 >
-                  User Name
+                  username
                 </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Sender name"
-                  id="name"
-                  autoComplete="name"
-                  className="form-input"
-                />
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
-
-              <div className="">
+              <div className="sm:col-span-2">
                 <label
-                  className="block text-gray-700 text-sm font-medium mb-2"
-                  for="name"
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   Email
                 </label>
-                <input
-                  type="text"
-                  name="email-address"
-                  placeholder="Sender Email"
-                  id="email-address"
-                  autoComplete="email"
-                  className="form-input"
-                />
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
 
               <div className="">
@@ -68,52 +69,55 @@ const PasswordForget = () => {
                   className="block text-gray-700 text-sm font-medium mb-2"
                   for="name"
                 >
-                  Enter OTP from email
+                  Enter OTP from Email
                 </label>
                 <input
                   type="text"
-                  name="email-address"
-                  placeholder="Receiver Email"
-                  id="email-address"
-                  autoComplete="email"
-                  className="form-input"
+                  name="otp-email"
+                  placeholder="otp-email"
+                  id="otp-email"
+                  autoComplete="otp-email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
 
-              <div className="">
+              <div className="sm:col-span-2">
                 <label
-                  className="block text-gray-700 text-sm font-medium mb-2"
-                  for="name"
+                  htmlFor="new-password"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   New Password
                 </label>
-                <input
-                  type="text"
-                  name=" Password"
-                  placeholder="Password"
-                  id=" Password"
-                  autoComplete=" Password"
-                  className="form-input"
-                />
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="new-password"
+                    id="new-password"
+                    autoComplete="new-password"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
 
-              <div className="">
+              <div className="sm:col-span-2">
                 <label
-                  className="block text-gray-700 text-sm font-medium mb-2"
-                  for="name"
+                  htmlFor="confirm-password"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   Confirm Password
                 </label>
-                <input
-                  type="text"
-                  name="Confirm Password"
-                  placeholder="Confirm Password"
-                  id="Confirm Password"
-                  autoComplete="Confirm Password"
-                  className="form-input"
-                />
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="confirm-password"
+                    id="confirm-password"
+                    autoComplete="confirm-password"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
             </div>
+
             <div className="px-4 py-2 text-center md:text-left sm:px-6">
               <button type="submit" className="btn-send">
                 Change password
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -243,6 +243,24 @@ const SignUp = () => {
               />
             </div>
           </div>
+
+          <div>
+            <label
+              htmlFor="otp-email"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Enter OTP from email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="otp-email"
+                id="otp-email"
+                autoComplete="otp-email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
         </div>
         <div className="mt-10">
           <button
