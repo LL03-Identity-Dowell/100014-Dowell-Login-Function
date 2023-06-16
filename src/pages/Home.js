12403ab713ac20312200206b9e2befa9eec7393b
Addisu <addisuhaile87@gmail.com>
2023-06-16 15:59:05 +0300

    Delete side and Tab components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import MyTabs from "../components/MyTabs";
-import TabsComponent from "../components/TabComponent";
 
 const Home = () => {
   return (
     <>
       <div>
-        {/* <TabsComponent /> */}
         <MyTabs />
       </div>
     </>
