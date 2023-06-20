d58e64c714481a73b7f0df2a69272ef8a6923953
Addisu <addisuhaile87@gmail.com>
2023-06-20 18:05:25 +0300

    Create faqs accordion

---
+++ src/components/AccordionData.js
@@ -0,0 +1,43 @@
+export const faqs = [
+  {
+    question: "Can I create more than one accounts?",
+    answer:
+      "Yes, you can create more than one account with the same E-mail address, however, you must use different usernames and login details.",
+  },
+  {
+    question: "Is it compulsory to add a photo when creating an account?",
+    answer:
+      "No. Adding a photo is optional. While creating an account, only focus on boxes marked with asterisks.",
+  },
+  {
+    question: "Should I write my phone number together with the country code?",
+    answer:
+      "No, please. Before entering your phone number, choose your country code from the dropdown under the “Country code” section.",
+  },
+  {
+    question: "How do I join as a member?",
+    answer:
+      "Joining is pretty easy. First, you need to download the Dowell UX Living Lab software and add it as an extension to your browser. Open the extension, click on the Dowell logo, Next click on login and finally click on join as a new member.",
+  },
+  {
+    question: "What if I can’t find my country code?",
+    answer:
+      "Countries are abbreviated with three letters and the country code beside it. For instance, if you are from Nigeria, your country code will appear in this format NGA (+234).",
+  },
+  {
+    question: "What if I didn’t receive an OTP?",
+    answer:
+      "Sometimes, emails containing OTP might appear in the spam section of your email. Be sure to check your spam folder, refresh your inbox, or wait a little more to get the OTP.",
+  },
+  {
+    question: "What should I do if I forget my password?",
+    answer:
+      "If you cannot remember your password, you will need to create a new password. Follow these steps to create a new password. From the login page, just below the “Do not remember the password”, click on “Click here”. Enter your username and E-mail, then click on “Get OTP”. An OTP will be sent to your email. Copy the OTP and paste it into the box provided for OTP. Then proceed to create a new password you can remember. Re-enter the password in the “Confirm password” box, and click on “Save password”.",
+  },
+  {
+    question:
+      "Why am I unable to log in even after inputting the correct username and password?",
+    answer:
+      "If you are sure the login details you entered are correct, you may need to accept the “Privacy Policy”.",
+  },
+];
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { faqs } from "../components/AccordionData";
 
 const FAQ = () => {
+  const [activeTab, setActiveTab] = useState(null);
+  const toggleAccordion = (index) => {
+    setActiveTab((prevTab) => (prevTab === index ? null : index));
+  };
+
   return (
-    <div>
-      FAQ
-      {/*  TODO: Add FAQ */}
+    <div className="container mx-auto max-w-4xl px-4 py-8">
+      <h1 className="text-2xl font-semibold mb-4">
+        Frequently Asked Questions
+      </h1>
+      <div>
+        {faqs.map((faq, index) => (
+          <div className="mb-4" key={faq.id}>
+            <button
+              className="flex justify-between w-full p-3 bg-gradient-to-r from-cyan-200 to-green-400 rounded-md focus:outline-none"
+              onClick={() => toggleAccordion(index)}
+            >
+              <span className="font-medium">{faq.question}</span>
+              <span>{activeTab === index ? "-" : "+"}</span>
+            </button>
+            {activeTab === index && (
+              <div className="px-4 py-2 bg-gray-100 rounded-md">
+                <p className="text-gray-800 leading-snug font-normal">
+                  {faq.answer}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
