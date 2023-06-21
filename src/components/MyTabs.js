69188855cd461d68e82b5be09315710d0f8d1958
Addisu <addisuhaile87@gmail.com>
2023-06-21 15:06:51 +0300

    Update tab icons by adding react-icons

--- src/components/MyTabs.js
+++ src/components/MyTabs.js
@@ -5,6 +5,7 @@ import Chat from "../pages/Chat";
 import Policy from "../pages/Policy";
 import Help from "../pages/Help";
 import FAQ from "../pages/FAQ";
+import { getCategoryIcon } from "./getCategoryIcon";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -48,22 +49,28 @@ const MyTabs = () => {
     <Tab.Group>
       <div className="w-full max-w-7xl mx-auto px-2 py-16 sm:px-0">
         <Tab.List className="flex space-x-1 rounded-xl bg-green-900/20 p-1">
-          {Object.keys(categories).map((category) => (
-            <Tab
-              key={category}
-              className={({ selected }) =>
-                classNames(
-                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-green-700",
-                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-green-400 focus:outline-none focus:ring-2",
-                  selected
-                    ? "bg-white shadow"
-                    : "text-green-100 hover:bg-white/[0.12] hover:text-white"
-                )
-              }
-            >
-              {category}
-            </Tab>
-          ))}
+          {Object.keys(categories).map((category) => {
+            const Icon = getCategoryIcon(category);
+            return (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-green-700",
+                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-green-400 focus:outline-none focus:ring-2",
+                    selected
+                      ? "bg-white shadow"
+                      : "text-green-100 hover:bg-white/[0.12] hover:text-white"
+                  )
+                }
+              >
+                <div className="flex items-center justify-center space-x-1">
+                  {Icon && <Icon className="w-5 h-5" />}
+                  <span>{category}</span>
+                </div>
+              </Tab>
+            );
+          })}
         </Tab.List>
         <Tab.Panels className="mt-2">
           {Object.values(categories).map((posts, idx) => (
@@ -81,6 +88,9 @@ const MyTabs = () => {
                     className="relative rounded-md p-3 hover:bg-gray-100"
                   >
                     <h3 className="text-sm font-medium leading-5">
+                      <span className="mr-1">
+                        {getCategoryIcon(post.category)}
+                      </span>
                       {post.content}
                     </h3>
                   </li>
