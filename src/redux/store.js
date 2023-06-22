c3e3a1353f4af0c01f3255ddb325224dbbbcdf99
Addisu <addisuhaile87@gmail.com>
2023-06-22 20:40:57 +0300

    add state to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "./countriesSlice";
 import passwordReducer from "./passwordSlice";
+import userReducer from "./userSlice";
 
 const store = configureStore({
   reducer: {
     countries: countriesReducer,
     password: passwordReducer,
+    username: userReducer,
   },
 });
 
