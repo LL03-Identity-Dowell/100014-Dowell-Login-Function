aaa0d9837e8e6e7eadda835e0a6c567fe6557f73
Addisu <addisuhaile87@gmail.com>
2023-07-08 17:26:58 +0300

    Update styling for mobile version of splash page

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import DoWellVerticalLogo from "../assets/images/Dowell-logo-Vertical.jpeg";
 
 const SplashPage = () => {
   const navigate = useNavigate();
@@ -16,27 +17,37 @@ const SplashPage = () => {
 
   return (
     <div className="antialiased bg-gray-100 flex items-center justify-center h-screen">
-      <div className="w-full space-y-4 md:space-y-8 text-gray-500 bg-gray-50 shadow-lg max-w-3xl rounded-xl p-6">
-        <h1 className="text-2xl mb-4 text-center">
-          Welcome to our Splash Page!
-        </h1>
-        <p id="message" className="text-gray-600 mb-4 text-center">
-          User <strong className="underline text-green-500">{Username}</strong>
-          successfully registered!
-        </p>
-        <div className="flex flex-row items-center justify-center mt-20">
-          <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
+      <div className="w-full flex flex-col md:flex-row space-y-6 space-x-2 md:space-x-6 p-6 items-center justify-center  text-gray-500 bg-gray-50 drop-shadow-lg max-w-3xl rounded-xl">
+        <img
+          src={DoWellVerticalLogo}
+          alt="DoWell logo"
+          className="h-38 w-44 rounded-lg drop-shadow-sm bg-slate-100 "
+          width="60"
+          height="88"
+        />
+        <div className="min-w-0 relative flex-auto space-y-8">
+          <h1 className="font-bold text-xl text-slate-900 truncate text-center">
+            Welcome to UXLiving Lab!
+          </h1>
+          <p className="text-gray-600 mb-4 text-center">
+            User{" "}
+            <strong className="underline text-green-500">{Username}</strong>
+            successfully registered!
+          </p>
+          <div className="flex flex-row items-center justify-center mt-20">
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
