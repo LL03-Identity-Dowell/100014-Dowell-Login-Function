33a2015bd875670bf6606f57fc6452c02ce220d2
Addisu <addisuhaile87@gmail.com>
2023-07-08 17:00:16 +0300

    Update styling for splash page

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const SplashPage = () => {
@@ -14,25 +15,29 @@ const SplashPage = () => {
   const { Username } = useParams();
 
   return (
-    <div className="flex flex-col items-center mt-20">
-      <h1 className="text-2xl mb-4">Welcome to our Splash Page!</h1>
-      <p id="message" className="text-gray-600 mb-4">
-        User <strong className="underline text-green-500">{Username}</strong>
-        successfully registered!
-      </p>
-      <div className="flex flex-row items-center mt-20">
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleCancel}
-        >
-          Cancel
-        </button>
+    <div className="antialiased bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-full space-y-4 md:space-y-8 text-gray-500 bg-gray-50 shadow-lg max-w-3xl rounded-xl p-6">
+        <h1 className="text-2xl mb-4 text-center">
+          Welcome to our Splash Page!
+        </h1>
+        <p id="message" className="text-gray-600 mb-4 text-center">
+          User <strong className="underline text-green-500">{Username}</strong>
+          successfully registered!
+        </p>
+        <div className="flex flex-row items-center justify-center mt-20">
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
