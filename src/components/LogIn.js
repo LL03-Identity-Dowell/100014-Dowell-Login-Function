06837ccb9a0eb4da18dff7c0aa50c6c2b5523652
Addisu <addisuhaile87@gmail.com>
2023-07-12 16:35:54 +0300

    Update the login image for mobile and desktop versions

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -8,8 +8,10 @@ import { loginUser } from "../redux/loginSlice";
 import { getOperatingSystem, getDeviceType } from "../utils/deviceUtils";
 import Coordinate from "../utils/Coordinate";
 import { detectBrowser } from "../utils/browserUtils";
-import DoWellVerticalLogo from "../assets/images/Dowell-logo-Vertical.jpeg";
 import { Radio } from "react-loader-spinner";
+import DoWellVerticalLogo from "../assets/images/Dowell-logo-Vertical.jpeg";
+import sideImage from "../assets/images/sideImage.webp";
+import { useMediaQuery } from "react-responsive";
 
 const schema = yup.object().shape({
   username: yup
@@ -54,6 +56,9 @@ const LogIn = () => {
   // Use the detectBrowser
   const browserType = detectBrowser();
 
+  // Use media queries to determine the screen size
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   const handleUserInfo = async ({ username, password }) => {
     const userData = {
       username,
@@ -74,38 +79,45 @@ const LogIn = () => {
     <>
       <div className="antialiased bg-gray-100">
         <div className="flex w-full min-h-screen items-center justify-center">
-          <div className="flex flex-col justify-between md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-gray-50 w-full max-w-3xl p-8 md:p-10 rounded-xl shadow-lg text-gray-500 overflow-hidden">
+          <div className="flex flex-col justify-between md:flex-row md:space-x-4 space-y-6 md:space-y-0 bg-gray-50 w-full max-w-3xl p-4 md:p-6 rounded-xl shadow-lg text-gray-500 overflow-hidden">
+            {isMobile ? (
+              <img
+                src={DoWellVerticalLogo}
+                alt="DoWell logo"
+                className="h-28 w-28 rounded-full drop-shadow-md"
+              />
+            ) : (
+              <img
+                src={sideImage}
+                alt="DoWell logo"
+                className="h-72 w-24 drop-shadow-md"
+              />
+            )}
+
             <div className="flex flex-col space-y-8">
-              <div className="flex items-center mx-auto max-w-2xl justify-center space-x-2 px-2 sm:px-0">
-                <img
-                  src={DoWellVerticalLogo}
-                  alt="DoWell logo"
-                  className="h-28 w-38 rounded-full drop-shadow-sm"
-                />
-                <h2 className="text-xl font-semibold tracking-wide text-green-500 md:text-2xl">
-                  Member Login
-                </h2>
-              </div>
+              <h2 className="text-xl font-semibold tracking-wide text-green-500 md:text-2xl">
+                Member Login
+              </h2>
 
               <div className="flex flex-col space-y-2">
                 <p className="pt-2 text-gray-500 text-base">
                   Don't remember username and password?
                 </p>
                 <Link to="/password">
-                  <span className="text-green-600 text-center">Click here</span>
+                  <span className="text-green-500 text-base">Click here</span>
                 </Link>
               </div>
 
-              <div className="flex text-gray-500 text-base space-x-2">
-                <p>Don't have an account?</p>
+              <div className="flex text-gray-500 text-base">
+                <p>Don't have an account?</p> {""}
                 <Link to="/signup">
-                  <span className="text-green-600">Sign up</span>
+                  <span className="text-green-500 text-base">Sign up</span>
                 </Link>
               </div>
             </div>
 
             <div className="relative">
-              <div className="relative z-10 bg-gray-50 rounded-2xl drop-shadow-md p-8 text-gray-700 md:w-80">
+              <div className="relative z-10 bg-gray-50 rounded-2xl drop-shadow-md p-6 text-gray-700 md:w-80">
                 <form
                   className="flex flex-col space-y-4"
                   onSubmit={handleSubmit(handleUserInfo)}
