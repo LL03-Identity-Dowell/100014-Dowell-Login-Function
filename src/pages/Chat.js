65a52bbeca78bb6d436b11dd04e49d2c0d862a5d
Addisu <addisuhaile87@gmail.com>
2023-07-17 17:05:12 +0300

    Create a chat app using iframe

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Iframe from "react-iframe";
 
 const Chat = () => {
   return (
-    <div>
-      Chat
-      {/*  TODO: Add chat form */}
+    <div className="max-w-3xl space-y-2 flex flex-col items-center">
+      <h2 className="font-semibold text-lg text-white bg-green-500 px-6 py-1 rounded-3xl">
+        Chat with UX Living Lab
+      </h2>
+      <Iframe
+        url="https://100096.pythonanywhere.com/chat/login/?session_id=test"
+        width="100%"
+        height="350px"
+        id="chatAppFrame"
+        className="py-1 px-2"
+        display="initial"
+        position="relative"
+      />
     </div>
   );
 };
