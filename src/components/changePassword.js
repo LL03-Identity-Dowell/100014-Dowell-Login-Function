451007edf7cc223584d04c13763bd7244b8993a5
Addisu <addisuhaile87@gmail.com>
2023-07-29 23:28:10 +0300

    Update the reusable passwordInput component and changePassowrd

--- src/components/changePassword.js
+++ src/components/changePassword.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import DoWellVerticalLogo from "../assets/images/Dowell-logo-Vertical.jpeg";
 import { Radio } from "react-loader-spinner";
 import PasswordInput from "./passwordInput";
@@ -17,7 +17,11 @@ const schema = yup.object().shape({
       ["administrator", "uxlivinglab", "dowellresearch", "dowellteam", "admin"],
       "Username not allowed"
     ),
-  old_password: yup.string().required("Old Password is required"),
+  old_password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(99)
+    .required("Password is required"),
   new_password: yup
     .string()
     .min(8, "Password must be at least 8 characters")
@@ -31,12 +35,14 @@ const schema = yup.object().shape({
 
 const ChangePassword = () => {
   const [loading, setLoading] = useState(false);
-  const [newPassword, setNewPassword] = useState("");
 
   const {
     handleSubmit,
     register,
     formState: { errors },
+    setValue,
+    getValues,
+    reset,
   } = useForm({ resolver: yupResolver(schema) });
 
   const onSubmit = async (data) => {
@@ -44,10 +50,7 @@ const ChangePassword = () => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     console.log("Submitting form with data:", data);
     setLoading(false);
-  };
-
-  const onChangePassword = (name, value) => {
-    setNewPassword(value);
+    reset();
   };
 
   return (
@@ -73,110 +76,102 @@ const ChangePassword = () => {
           </div>
         </div>
 
-        <form
-          className="mx-auto mt-8 max-w-xl sm:mt-12"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div>
-            <label htmlFor="username" className="label">
-              Username <span className="text-red-500">*</span>
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Enter Your Username"
-                autoComplete="username"
-                className="input-field"
-                {...register("username")}
-              />
-              {errors?.username && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors?.username?.message}
-                </p>
-              )}
-            </div>
-          </div>
-
-          <div className="mt-2.5">
-            <label htmlFor="old_password" className="label">
-              Old Password <span className="text-red-500">*</span>
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="password"
-                name="old_password"
-                id="old_password"
-                placeholder="Enter old Password"
-                autoComplete="old_password"
-                className="input-field"
-                {...register("old_password")}
-              />
-              {errors.old_password && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.old_password.message}
-                </p>
-              )}
-            </div>
-          </div>
-
-          <div className="mt-2">
-            <PasswordInput
-              name="new_password"
-              value={newPassword}
-              errors={errors}
-              {...register("new_password")}
-              onChange={onChangePassword}
-            />
-          </div>
-
-          <div className="mt-2.5">
-            <label htmlFor="confirm_password" className="label">
-              Confirm Password <span className="text-red-500">*</span>
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="password"
-                name="confirm_password"
-                id="confirm_password"
-                placeholder="Confirm new Password"
-                autoComplete="confirm_password"
-                className="input-field"
-                {...register("confirm_password")}
-              />
-              {errors.confirm_password && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.confirm_password.message}
-                </p>
-              )}
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <button type="submit" className="submit-btn">
-              {loading ? (
-                <Radio
-                  visible={true}
-                  height={30}
-                  width={30}
-                  ariaLabel="radio-loading"
-                  wrapperStyle={{}}
-                  wrapperClassName="radio-wrapper"
-                  color="#1ff507"
+        <FormProvider {...register}>
+          <form
+            className="mx-auto mt-8 max-w-xl sm:mt-12"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <label htmlFor="username" className="label">
+                Username <span className="text-red-500">*</span>
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Enter Your Username"
+                  autoComplete="username"
+                  className="input-field"
+                  {...register("username")}
                 />
-              ) : (
-                "Change Password"
-              )}
-            </button>
-          </div>
+                {errors?.username && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors?.username?.message}
+                  </p>
+                )}
+              </div>
+            </div>
 
-          <div className="w-72 mx-auto flex items-center justify-center rounded-md bg-green-300 space-x-2 px-3.5 py-2.5 mt-8 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700">
-            <Link to="/" className="text-center">
-              Do have an account? Log in
-            </Link>
-          </div>
-        </form>
+            <div className="mt-2.5">
+              <label htmlFor="old_password" className="label">
+                Old Password <span className="text-red-500">*</span>
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="password"
+                  name="old_password"
+                  id="old_password"
+                  placeholder="Enter old Password"
+                  autoComplete="old_password"
+                  className="input-field"
+                  {...register("old_password")}
+                />
+                {errors.old_password && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.old_password.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-2">
+              <PasswordInput
+                name="new_password"
+                register={register}
+                value={getValues("new_password")}
+                errors={errors}
+                isConfirm={false}
+                onChange={setValue}
+              />
+            </div>
+
+            <div className="mt-2">
+              <PasswordInput
+                name="confirm_password"
+                register={register}
+                value={getValues("confirm_password")}
+                errors={errors}
+                isConfirm={true}
+                onChange={setValue}
+              />
+            </div>
+
+            <div className="mt-4">
+              <button type="submit" className="submit-btn">
+                {loading ? (
+                  <Radio
+                    visible={true}
+                    height={30}
+                    width={30}
+                    ariaLabel="radio-loading"
+                    wrapperStyle={{}}
+                    wrapperClassName="radio-wrapper"
+                    color="#1ff507"
+                  />
+                ) : (
+                  "Change Password"
+                )}
+              </button>
+            </div>
+
+            <div className="w-72 mx-auto flex items-center justify-center rounded-md bg-green-300 space-x-2 px-3.5 py-2.5 mt-8 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700">
+              <Link to="/" className="text-center">
+                Do have an account? Log in
+              </Link>
+            </div>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
--- src/components/passwordInput.js
+++ src/components/passwordInput.js
@@ -1,117 +1,129 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import zxcvbn from "zxcvbn";
 
-const PasswordInput = ({ name, value, errors, onChange }) => {
+// Password requirements
+const requirements = [
+  {
+    regex: /[a-z]/, // at least 1 lowercase letter
+    message: "Password must contain at least 1 lowercase letter",
+  },
+  {
+    regex: /[A-Z]/, // at least 1 uppercase letter
+    message: "Password must contain at least 1 uppercase letter",
+  },
+  {
+    regex: /\d/, // at least 1 digit
+    message: "Password must contain at least 1 digit",
+  },
+  {
+    regex: /[@$!%*?&]/, // at least 1 special character
+    message: "Password must contain at least 1 special character",
+  },
+  {
+    regex: /.{8}/, // at least 8 characters
+    message: "Password must be at least 8 characters",
+  },
+];
+
+const PasswordInput = ({ name, value, isConfirm, errors, onChange }) => {
+  const [password, setPassword] = useState(value || "");
   const [passwordStrength, setPasswordStrength] = useState(0);
   const [passwordMessage, setPasswordMessage] = useState("");
-  const passwordRef = useRef("");
-
-  // Password requirements
-  const requirements = [
-    {
-      regex: /[a-z]/, // at least 1 lowercase letter
-      message: "Password must contain at least 1 lowercase letter",
-    },
-    {
-      regex: /[A-Z]/, // at least 1 uppercase letter
-      message: "Password must contain at least 1 uppercase letter",
-    },
-    {
-      regex: /\d/, // at least 1 digit
-      message: "Password must contain at least 1 digit",
-    },
-    {
-      regex: /[@$!%*?&]/, // at least 1 special character
-      message: "Password must contain at least 1 special character",
-    },
-    {
-      regex: /.{8}/, // at least 8 characters
-      message: "Password must be at least 8 characters",
-    },
-  ];
 
   // Handle password change
-  const handlePasswordChange = () => {
-    const newPassword = passwordRef.current.value;
-    const { score, feedback } = zxcvbn(newPassword);
-
-    const validationMessages = requirements.map((requirement) => {
-      const isValid = requirement.regex.test(newPassword);
-      return {
-        message: requirement.message,
-        isValid,
-      };
-    });
-
-    const isValidPassword = validationMessages.every(
-      (message) => message.isValid
-    );
-    const passwordFeedback = feedback.warning || feedback.suggestions[0];
-
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
     onChange(name, newPassword);
 
-    setPasswordStrength(isValidPassword ? score : 0);
-    setPasswordMessage(isValidPassword ? passwordFeedback : validationMessages);
+    if (!isConfirm) {
+      const { score, feedback } = zxcvbn(newPassword);
+
+      const validationMessages = requirements.map((requirement) => {
+        const isValid = requirement.regex.test(newPassword);
+        return {
+          message: requirement.message,
+          isValid,
+        };
+      });
+
+      const isValidPassword = validationMessages.every(
+        (message) => message.isValid
+      );
+
+      const passwordFeedback = feedback.warning || feedback.suggestions[0];
+
+      setPasswordStrength(isValidPassword ? score : 0);
+      setPasswordMessage(
+        isValidPassword ? passwordFeedback : validationMessages
+      );
+    }
   };
 
   return (
-    <div>
-      <label htmlFor="Password" className="label">
-        New Password <span className="text-red-500">*</span>
-      </label>
-      <div className="mt-2.5 relative">
-        <input
-          name={name}
-          type="password"
-          id={name}
-          placeholder="Enter new Password"
-          autoComplete="Password"
-          className="input-field"
-          value={value}
-          ref={passwordRef}
-          onChange={handlePasswordChange}
-        />
-        {errors[name] && (
-          <p className="text-red-500 text-xs mt-1">{errors[name].message}</p>
+    <>
+      <div>
+        <label htmlFor="Password" className="label">
+          {isConfirm ? "Confirm Password" : "New Password"}
+          <span className="text-red-500">*</span>
+        </label>
+        <div className="mt-2.5 relative">
+          <input
+            name={name}
+            type="password"
+            id={name}
+            placeholder={`Enter ${isConfirm ? "confirm" : "new"} Password`}
+            autoComplete={isConfirm ? "confirm_password" : "new_password"}
+            className="input-field"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          {errors[name] && (
+            <p className="text-red-500 text-xs mt-1">{errors[name].message}</p>
+          )}
+        </div>
+        {!isConfirm && (
+          <>
+            <div className="h-2 bg-gray-300 rounded overflow-hidden w-11/12">
+              <div
+                className={`h-full strength-${passwordStrength} ${
+                  passwordStrength === 0
+                    ? "bg-red-500"
+                    : passwordStrength === 1
+                    ? "bg-orange-400"
+                    : passwordStrength === 2
+                    ? "bg-yellow-400"
+                    : "bg-green-500"
+                }`}
+                style={{ width: `${(passwordStrength / 4) * 100}%` }}
+              ></div>
+            </div>
+            {Array.isArray(passwordMessage) ? (
+              <div>
+                {passwordMessage.map((message, index) => (
+                  <p
+                    key={index}
+                    className={`text-xs mt-1 ${
+                      message.isValid ? "text-green-500" : "text-red-500"
+                    }`}
+                  >
+                    {message.message}
+                  </p>
+                ))}
+              </div>
+            ) : (
+              <p
+                className={`text-xs mt-1 ${
+                  passwordMessage ? "text-green-500" : "text-red-500"
+                }`}
+              >
+                {passwordMessage}
+              </p>
+            )}
+          </>
         )}
       </div>
-      <div className="h-2 bg-gray-300 rounded overflow-hidden w-11/12">
-        <div
-          className={`h-full strength-${passwordStrength} ${
-            passwordStrength === 0
-              ? "bg-red-500"
-              : passwordStrength === 1
-              ? "bg-orange-400"
-              : passwordStrength === 2
-              ? "bg-yellow-400"
-              : "bg-green-500"
-          }`}
-          style={{ width: `${(passwordStrength / 4) * 100}%` }}
-        ></div>
-      </div>
-      {Array.isArray(passwordMessage) ? (
-        <div>
-          {passwordMessage.map((message, index) => (
-            <p
-              key={index}
-              className={`text-xs mt-1 ${
-                message.isValid ? "text-green-500" : "text-red-500"
-              }`}
-            >
-              {message.message}
-            </p>
-          ))}
-        </div>
-      ) : (
-        <p
-          className={`text-xs mt-1 ${
-            passwordMessage ? "text-green-500" : "text-red-500"
-          }`}
-        >
-          {passwordMessage}
-        </p>
-      )}
-    </div>
+    </>
   );
 };
 
