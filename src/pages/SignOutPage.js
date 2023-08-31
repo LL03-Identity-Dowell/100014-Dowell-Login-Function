cf87c47e15289704febceba2e83712a4669a30ad
Addisu <addisuhaile87@gmail.com>
2023-08-31 16:43:11 +0300

    Update path for signOut redirect to login

--- src/pages/SignOutPage.js
+++ src/pages/SignOutPage.js
@@ -86,7 +86,9 @@ const SignOutPage = () => {
                 <div className="text-center">
                   <p className="text-green-500">{loggedOut}</p>
                   <div className="w-72 mx-auto flex items-center justify-center rounded-md bg-green-300 space-x-2 px-3.5 py-2.5 mt-8 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700">
-                    <Link to={`/?redirect_url=${redirectUrl}`}>Log in</Link>
+                    <Link to={`/beta/?redirect_url=${redirectUrl}`}>
+                      Log in
+                    </Link>
                   </div>
                 </div>
               ) : (
