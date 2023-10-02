e765a4cb3e3ecaa7fa0261fe427f5a8bf480abb9
oyebodemax <bodokunfolami@gmail.com>
2023-10-02 14:28:51 +0100

    made changes to core/urls route /beta

--- core/urls.py
+++ core/urls.py
@@ -24,10 +24,10 @@ from server.views import error_404, error_500
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    # path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('beta/ ', TemplateView.as_view(template_name='index.html'))
+    path('beta/', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
@@ -38,8 +38,6 @@ urlpatterns += i18n_patterns(
 )
 
 if settings.DEBUG:
-    import mimetypes
-    mimetypes.add_type("application/javascript", ".js", True)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
