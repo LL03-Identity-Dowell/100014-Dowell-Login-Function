6774331a86f4594579fcf7e21ec1899072a020e2
bodeokunfolami <bodokunfolami@gmail.com>
2023-10-04 13:52:00 +0100

    Built react files

--- core/urls.py
+++ core/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     # path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('beta/', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
