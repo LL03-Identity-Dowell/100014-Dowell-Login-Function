66515d78950064f301cb6c03d50fcd8ca3f27b60
Addisu <addisuhaile87@gmail.com>
2023-10-04 10:11:51 +0300

    Fix the responsiveness of help video

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -2,7 +2,7 @@ import Iframe from "react-iframe";
 
 const FAQ = () => {
   return (
-    <div className="max-w-3xl space-y-2 flex flex-col items-center">
+    <div className="max-w-3xl space-y-2 flex flex-col items-center overflow-hidden">
       <Iframe
         url="https://uxlivinglab.com/en/faq/"
         width="100%"
--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,18 +1,24 @@
 import React from "react";
-import YouTube from "react-youtube";
 
 const Help = () => {
   const videoId = "PSmX-A5Cn_E";
   const videoTitle = "Watch a 2 minute video on UX living Lab";
 
   return (
-    <div className="relative">
-      <div className="w-full flex justify-center items-center">
-        <div className="max-w-3xl space-y-2 flex flex-col items-center">
+    <div className="container mx-auto p-2">
+      <div className="w-full overflow-hidden">
+        <div className=" flex flex-col items-center space-y-2">
           <h2 className="font-semibold text-lg text-white bg-green-500 px-6 py-1 rounded-3xl">
             {videoTitle}
           </h2>
-          <YouTube videoId={videoId} />
+          <div className="aspect-video w-full">
+            <iframe
+              title="YouTube Video"
+              className="w-full h-full rounded-lg"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              allowFullScreen
+            />
+          </div>
         </div>
       </div>
     </div>
