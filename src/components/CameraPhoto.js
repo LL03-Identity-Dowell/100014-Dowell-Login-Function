a6bf6cfc6d380172f9837a556867179ff7a8e6c6
Addisu <addisuhaile87@gmail.com>
2023-12-05 17:03:46 +0300

    Modified the display of captured or retake photo

--- src/components/CameraPhoto.js
+++ src/components/CameraPhoto.js
@@ -13,36 +13,50 @@ const CameraPhoto = () => {
       toast.success("Photo captured successfully 🚀");
     } catch (error) {
       toast.error("An error occurred.");
-      console.log("Error capture photo:", error);
+      console.log("Error capturing photo:", error);
     }
   }, [webcamRef, setImgSrc]);
+
+  const handleRetake = () => {
+    setImgSrc(null);
+  };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-50">
       <div className="bg-white rounded-xl p-2">
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={320}
-          height={240}
-          className="mb-4 rounded-md"
-        />
-        <button
-          type="button"
-          className="w-full h-12 px-6 font-semibold rounded-md border bg-green-500 hover:bg-green-700 text-white"
-          onClick={handleCapture}
-        >
-          Capture Photo
-        </button>
-        {imgSrc && (
+        {imgSrc ? (
           <div className="mt-4">
             <img
               src={imgSrc}
-              alt="img"
+              alt="CapturedPhoto"
               className="w-full h-auto rounded-md shadow-md"
             />
+            <button
+              type="button"
+              className="w-full h-12 mt-2 px-6 font-semibold rounded-md border bg-green-500 hover:bg-green-700 text-white"
+              onClick={handleRetake}
+            >
+              Retake Photo
+            </button>
           </div>
+        ) : (
+          <>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              width={320}
+              height={240}
+              className="mb-4 rounded-md"
+            />
+            <button
+              type="button"
+              className="w-full h-12 px-6 font-semibold rounded-md border bg-green-500 hover:bg-green-700 text-white"
+              onClick={handleCapture}
+            >
+              Capture Photo
+            </button>
+          </>
         )}
       </div>
     </div>
