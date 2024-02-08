a6a127a1706cf401a7931870e73af1514cdc2031
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-02-08 17:30:37 +0300

    record voice and show it in an audio player

--- src/components/AudioLogin.js
+++ src/components/AudioLogin.js
@@ -1,43 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaFileUpload } from "react-icons/fa";
-import { toast } from "react-toastify";
 import { ReactMic } from "react-mic";
-
+import AudioPlayer from "../utils/AudioPlayer";
 const AudioLogin = () => {
   const [isRecording, setIsRecording] = useState(false);
+  const [voiceData, setVoiceData] = useState("");
 
-  const handleAudioUpload = async (audioData) => {
-    try {
-      // Handle the recorded audio data (e.g., upload to server)
-      toast.success("Audio uploaded successfully 🚀");
-    } catch (error) {
-      toast.error("An error occurred during audio upload.");
-      console.log("Error during audio upload:", error);
-    }
+  const handleAudioUpload = (audioData) => {
+    setVoiceData(audioData);
+    setIsRecording(false);
   };
 
   const handleStartRecording = () => {
     setIsRecording(true);
+    setVoiceData("");
   };
 
   const handleStopRecording = () => {
     setIsRecording(false);
   };
-
+  useEffect(() => {
+    console.log("This is is recording", isRecording);
+  }, [isRecording]);
   return (
     <div className="flex flex-col justify-center items-center space-y-4 mx-auto">
-      <div className="max-w-screen-md p-4 border border-dashed border-gray-300 rounded-lg bg-gray-50 flex flex-col items-center justify-center">
-        <ReactMic
-          record={isRecording}
-          className="sound-wave"
-          onStop={(audioData) => handleAudioUpload(audioData)}
-        />
+      <div className="w-full max-w-md p-4 border border-dashed border-gray-300 rounded-lg bg-gray-50 flex flex-col items-center justify-center ">
+        {voiceData ? (
+          <AudioPlayer url={voiceData.blobURL} />
+        ) : (
+          !voiceData && (
+            <ReactMic
+              record={isRecording}
+              className={"mediaplayer"}
+              onStop={(audioData) => {
+                handleAudioUpload(audioData);
+              }}
+            />
+          )
+        )}
+
         <button
           onClick={isRecording ? handleStopRecording : handleStartRecording}
           type="button"
           className={`${
             isRecording ? "bg-red-500" : "bg-green-500"
-          } flex items-center justify-center h-10 px-6 font-semibold rounded-md border hover:bg-green-700 text-white`}
+          } flex items-center justify-center h-10 px-6 font-semibold rounded-md border hover:bg-green-700 text-white `}
         >
           {isRecording ? "Stop Recording" : "Start Recording"}
         </button>
