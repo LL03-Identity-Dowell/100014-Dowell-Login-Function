bbbd177a2115b6d361b7cfc4f5cbcdbe0223272a
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-03-21 12:33:33 +0300

    add spinner to  help when iframe is loading

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { useState } from "react";
 const Help = () => {
+  const [helpLoading, setHelpLoading] = useState(true);
   const videoId = "PSmX-A5Cn_E";
   const videoTitle = "Watch a 2 minute video on UX living Lab";
 
@@ -11,12 +12,23 @@ const Help = () => {
           <h2 className="font-semibold text-lg text-white bg-green-500 px-6 py-1 rounded-3xl">
             {videoTitle}
           </h2>
+          {helpLoading && (
+            <div className="policyFrameSpinner iframespinner">
+              {" "}
+              <div className="box" style={{ width: "50px", height: "50px" }}>
+                {" "}
+              </div>
+            </div>
+          )}
           <div className="aspect-video w-full">
             <iframe
               title="YouTube Video"
               className="w-full h-full rounded-lg"
               src={`https://www.youtube.com/embed/${videoId}`}
               allowFullScreen
+              onLoad={() => {
+                setHelpLoading(false);
+              }}
             />
           </div>
         </div>
