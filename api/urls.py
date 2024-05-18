b88e0b76afb49cf16f6d77ad38c6d5155db8eacd
Nagarajuvuliki <87876269+Nagarajuvuliki@users.noreply.github.com>
2024-05-18 22:13:30 +0530

    add otp verify and routes of registration

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from api.views import *
 urlpatterns = [
     path("register",register,name="register"),
+    path('mobilesms/',mobilesms, name="mobilesms"),
+    path('mobile_otp/',mobile_otp, name='mobile_otp_api'),
+    path('emailotp/',email_otp, name='email_otp_api'),
+    path('otp_verify/',otp_verify, name="otp_verify"),
 ]
